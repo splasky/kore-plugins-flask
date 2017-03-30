bb954d136a0d11de6fd12c404b0df44324df8296
Artur Maciag <artur@MacBook-Pro-Artur.local>
2017-03-30 17:20:32 +0200

    initial version

---
+++ kore_plugins_flask/plugins/application.py
@@ -0,0 +1,33 @@
+import logging
+
+from flask import Flask
+
+from kore.components.plugins.base import BasePluginComponent
+
+log = logging.getLogger(__name__)
+
+
+class FlaskApplicationPluginComponent(BasePluginComponent):
+
+    def get_services(self):
+        return (
+            ('config', self.config),
+            ('application', self.application),
+        )
+
+    def config(self, container):
+        config = container('config')
+
+        return config.get('flask', {})
+
+    def application(self, container):
+        config = container('kore.components.flask.config')
+
+        instance_path = config.get('instance_path', None)
+        root_path = config.get('root_path', None)
+
+        return Flask(
+            __name__,
+            instance_path=instance_path,
+            root_path=root_path,
+        )
